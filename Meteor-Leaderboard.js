2534294ea3afa2779c67cf98d90bd6e552106c5c
Frank Christiaan Peelen <frank.peelen@yahoo.com>
2015-05-07 22:31:10 +0900

    Finish chapter 5: Templates

--- Meteor-Leaderboard.js
+++ Meteor-Leaderboard.js
@@ -1,1 +1,16 @@
 PlayersList = new Mongo.Collection('players');
+
+console.log("Hello world");
+
+if(Meteor.isClient){
+  console.log("Hello client");
+  Template.leaderboard.helpers({
+    'player': function(){
+      return PlayersList.find()
+    }
+  });
+}
+
+if(Meteor.isServer){
+  console.log("Hello server");
+}
