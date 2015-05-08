a119ae2a87dd945c36e072cdf6b23d5acd9eb7d4
Frank Christiaan Peelen <frank.peelen@yahoo.com>
2015-05-08 09:20:32 +0900

    Finish chapter 8: Databases, Part 2

--- Meteor-Leaderboard.js
+++ Meteor-Leaderboard.js
@@ -7,7 +7,7 @@ if(Meteor.isClient){
 
   Template.leaderboard.helpers({
     'player': function(){
-      return PlayersList.find();
+      return PlayersList.find({}, {sort: {score: -1, name: 1} })
     },
     'selectedClass': function(){
       var playerId = this._id;
@@ -15,6 +15,10 @@ if(Meteor.isClient){
       if(playerId == selectedPlayer){
         return "selected";
       }
+    },
+    'showSelectedPlayer': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      return PlayersList.findOne(selectedPlayer);
     }
   });
 
@@ -23,6 +27,14 @@ if(Meteor.isClient){
       var playerID = this._id;
       Session.set('selectedPlayer', playerID);
       var selectedPlayer = Session.get('selectedPlayer');
+    },
+    'click .increment': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      PlayersList.update(selectedPlayer, {$inc: {score: 5} });
+    },
+    'click .decrement': function(){
+      var selectedPlayer = Session.get('selectedPlayer');
+      PlayersList.update(selectedPlayer, {$inc: {score: -5} });
     }
   });
 }
